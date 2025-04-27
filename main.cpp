50289e07c55ac408f4f04d9593ee3901870ca2da
Abdul Basit <contact.abdulbasit.cs@gmail.com>
2025-04-27 21:01:48 +0500

    update the code

--- main.cpp
+++ main.cpp
@@ -3,90 +3,112 @@
 #include <string>
 using namespace std;
 
-struct Transitions
+struct Transition
 {
-    char O; // Origin state
-    char F; // Final state
-    char I; // Input symbol
-    char A; // Stack top symbol
-    char D; // Push symbol
+    char fromState;
+    char toState;
+    char inputSymbol;
+    char stackTop;
+    char pushSymbol;
 };
 
-bool simulatePDA(string input, Transitions transitions[], int N, char startState, char acceptState)
+bool simulatePDA(string input, Transition transitions[], int numTransitions, char startState, char acceptState)
 {
-    stack<char> PDAStack;
-    PDAStack.push('$');
+    stack<char> pdaStack;
+    pdaStack.push('$');
     char currentState = startState;
-    for (int i = 0; i < input.length(); i++)
+    int inputIndex = 0;
+
+    while (true)
     {
-        char inputSymbol = input[i];
-        int transitionFound = 0;
-        for (int j = 0; j < N; j++)
+        bool transitionFound = false;
+
+        for (int i = 0; i < numTransitions; i++)
         {
-            if (transitions[j].O == currentState && (transitions[j].I == inputSymbol || transitions[j].I == 'e'))
+            Transition t = transitions[i];
+
+            bool inputMatches = (t.inputSymbol == 'e' || (inputIndex < input.length() && t.inputSymbol == input[inputIndex]));
+            bool stackMatches = (t.stackTop == 'e' || (!pdaStack.empty() && t.stackTop == pdaStack.top()));
+
+            if (t.fromState == currentState && inputMatches && stackMatches)
             {
-                currentState = transitions[j].F;
-                if (transitions[j].D != 'e')
+                currentState = t.toState;
+
+                if (t.stackTop != 'e' && !pdaStack.empty())
                 {
-                    PDAStack.push(transitions[j].A);
+                    pdaStack.pop();
                 }
-                if (transitions[j].A != 'e')
+
+                if (t.pushSymbol != 'e')
                 {
-                    PDAStack.pop();
+                    pdaStack.push(t.pushSymbol);
                 }
-                transitionFound = 1;
+
+                if (t.inputSymbol != 'e')
+                {
+                    inputIndex++;
+                }
+
+                transitionFound = true;
                 break;
             }
         }
+
         if (!transitionFound)
         {
-            cout << "No transition found for input symbol " << inputSymbol << " in state " << currentState << endl;
-            return false;
+            break;
+        }
+
+        if (inputIndex == input.length())
+        {
+            bool epsilonTransitionExists = false;
+            for (int i = 0; i < numTransitions; i++)
+            {
+                if (transitions[i].fromState == currentState && transitions[i].inputSymbol == 'e')
+                {
+                    epsilonTransitionExists = true;
+                    break;
+                }
+            }
+            if (!epsilonTransitionExists)
+            {
+                break;
+            }
         }
     }
-    if (PDAStack.top() == '$')
+
+    if (!pdaStack.empty() && pdaStack.top() == '$')
     {
-        PDAStack.pop();
+        pdaStack.pop();
     }
-    return currentState == acceptState && PDAStack.empty();
+    return (inputIndex == input.length()) && (currentState == acceptState) && pdaStack.empty();
 }
 
 int main()
 {
-    int N;
-    // cout << "Enter number of transitions: ";
-    // cin >> N;
+    int numTransitions = 4;
+    Transition transitions[] = {
+        {'A', 'A', 'a', 'e', 'A'},
+        {'A', 'B', 'b', 'A', 'e'},
+        {'B', 'B', 'b', 'A', 'e'},
+        {'B', 'C', 'e', '$', 'e'}};
 
-    // Transitions transition[N];
-    // cout << "For each transition, enter:\n";
-    // cout << "Origin state, Final state, Input symbol (e for epsilon),\n";
-    // cout << "Stack top symbol, Push symbol (e for no push)\n";
+    for (int i = 0; i < numTransitions; i++)
+    {
+        cout << transitions[i].fromState << " " << transitions[i].toState << " "
+             << transitions[i].inputSymbol << " " << transitions[i].stackTop << " "
+             << transitions[i].pushSymbol << endl;
+    }
 
-    // for (int i = 0; i < N; i++) {
-    //     cout << "Enter transition " << i + 1 << ": ";
-    //     cin >> transition[i].O >> transition[i].F >> transition[i].I
-    //         >> transition[i].A >> transition[i].D;
-    // }
-    N = 5;
-    Transitions transition[N] = {
-        {'A', 'B', 'e', '$', 'a'},
-        {'B', 'B', 'a', 'a', 'a'},
-        {'B', 'C', 'b', 'a', 'a'},
-        {'C', 'C', 'b', 'a', 'a'},
-        {'C', 'D', 'e', 'e', 'e'}};
-
-    char startState, acceptState;
-    // cout << "Enter start state: ";
-    // cin >> startState;
-    // cout << "Enter accept state: ";
-    // cin >> acceptState;
-    startState = 'A';
-    acceptState = 'C';
+    char startState = 'A';
+    char acceptState = 'C';
     string input;
+
     cout << "Enter input string: ";
     cin >> input;
 
-    bool accepted = simulatePDA(input, transition, N, startState, acceptState);
+    bool accepted = simulatePDA(input, transitions, numTransitions, startState, acceptState);
+
     cout << "String is " << (accepted ? "ACCEPTED" : "REJECTED") << endl;
 
     return 0;
